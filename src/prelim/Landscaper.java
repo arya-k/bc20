68b92b61b9a3e1e4eca0edb821dea6f9f71e02ed
ajng <ajng@umich.edu>
2020-01-08 17:28:15 -0500

    Added a few function headers for landscaper

--- src/prelim/Landscaper.java
+++ src/prelim/Landscaper.java
@@ -11,4 +11,17 @@ class Landscaper extends Robot {
     @Override
     public void onUpdate() throws GameActionException {
     }
+
+    public void landscapeAt(MapLocation location, int height) throws GameActionException { // Landscape at a certain point
+
+    }
+
+    public void landscapeAround(MapLocation location, int height) throws GameActionException { // Landscape around a building at point 'location'
+
+    }
+
+    public void landscapeInShape() throws GameActionException { // Landscape in given shape
+
+    }
+
 }
