30babb8f5094e9d127edc497671fa5028b98e888
Arya Kumar <thearyaskumar@icloud.com>
2020-01-10 17:32:18 -0500

    added nothingplayer

---
+++ src/donothing/RobotPlayer.java
@@ -0,0 +1,8 @@
+package donothing;
+import battlecode.common.*;
+
+public strictfp class RobotPlayer {
+    public static void run(RobotController rc) throws GameActionException {
+        Clock.yield()
+    }
+}
