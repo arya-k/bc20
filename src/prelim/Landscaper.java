a5ba741791b0e21ae3bcfd270b2904dc7cb248dd
ajng <ajng@umich.edu>
2020-01-10 01:13:48 -0500

    More landscaper functionality

--- src/prelim/Landscaper.java
+++ src/prelim/Landscaper.java
@@ -17,17 +17,19 @@ class Landscaper extends Unit {
                 (height > RobotType.LANDSCAPER.dirtLimit && rc.getDirtCarrying() == RobotType.LANDSCAPER.dirtLimit)) {
 
             MapLocation[] adjLocations = arrayOfAdjLocations(location, location.directionTo(rc.getLocation())); //input this into the pathfinder as the point B to test
-            if (true) { // travel to location
-                for (int i = 0; i < 8; ++i) {
-                    if (true) { // travel to point adjLocations[i]
-                        break;
+            if (!rc.getLocation().isAdjacentTo(location)) {
+                if (true) { // travel to location
+                    for (int i = 0; i < 8; ++i) {
+                        if (true) { // travel to point adjLocations[i]
+                            break;
+                        }
                     }
-                }
-            } else {
-                buildPathTo(location);
-                for (int i = 0; i < 8; ++i) {
-                    if (true) { // travel to point adjLocations[i]
-                        break;
+                } else {
+                    buildPathTo(location);
+                    for (int i = 0; i < 8; ++i) {
+                        if (true) { // travel to point adjLocations[i]
+                            break;
+                        }
                     }
                 }
             }
@@ -47,10 +49,18 @@ class Landscaper extends Unit {
     }
 
     public void landscapeAround(MapLocation location, int height) throws GameActionException { // Landscape around a building at point 'location'
-
+        MapLocation[] adjLocations = arrayOfAdjLocations(location, location.directionTo(rc.getLocation()));
+        for (int i = 0; i < 8; ++i) {
+            landscapeAt(adjLocations[i], height);
+        }
     }
 
     public void landscapeInShape(FastLocSet shape) throws GameActionException { // Landscape in given shape
+
+    }
+
+    public void digHoleAt(MapLocation location, int height) throws GameActionException { // Digs hole at location
+
 
     }
 
@@ -62,15 +72,24 @@ class Landscaper extends Unit {
         if (true) { // if landscaper can already reach that location -- using the pathfinding function
             return;
         } else {
-
+            while (rc.getDirtCarrying() < RobotType.LANDSCAPER.dirtLimit) {
+                int elevationDiff = rc.senseElevation(rc.getLocation()) - rc.senseElevation(rc.getLocation().add(rc.getLocation().directionTo(location)));
+                if (elevationDiff < -3) {
+                    landscapeAt(rc.getLocation().add(rc.getLocation().directionTo(location)), elevationDiff + 3);
+                } else if (elevationDiff > 3) {
+                    digHoleAt(rc.getLocation().add(rc.getLocation().directionTo(location)), elevationDiff - 3);
+                }
+                rc.move(rc.getLocation().directionTo(location));
+            }
         }
     }
 
     public MapLocation[] arrayOfAdjLocations(MapLocation location, Direction direction) throws GameActionException {
+        // Returns an array of MapLocations surrounding a certain point
         MapLocation[] adjLocations = new MapLocation[8];
         for (int i = 0; i < 8; ++i) {
             adjLocations[i] = location.add(direction);
-            direction.rotateRight(); // not sure if we should rotate left or right first
+            direction.rotateRight(); // not sure if we should rotate left or right
         }
         return adjLocations;
     }
