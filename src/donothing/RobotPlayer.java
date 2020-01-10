a86de2a8a92bb867c67d1922bae9f10f316b6460
Arya Kumar <thearyaskumar@icloud.com>
2020-01-10 18:00:17 -0500

    fixed donothing again :(

--- src/donothing/RobotPlayer.java
+++ src/donothing/RobotPlayer.java
@@ -3,6 +3,6 @@ import battlecode.common.*;
 
 public strictfp class RobotPlayer {
     public static void run(RobotController rc) throws GameActionException {
-        Clock.yield();
+        while (true) Clock.yield();
     }
 }
